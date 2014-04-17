b6ff1a6ea3954334723acd8d91bbd8a5cd577e21
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2014-04-17 05:40:14 -0700

    Initial application with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AncestralResearch::Application.config.secret_token = 'fa79a4fd67b670b2b7348cb5848f4751282b83fc9cd446245760e015da8f60dbb5e9fb7d3ab7f3e4e24cf73eb6bdc965ada8231e1a37cd41a70df40661094a14'
