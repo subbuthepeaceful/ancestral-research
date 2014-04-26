374b85e285f46a7f1046f3383e12291502c9685d
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2014-04-26 06:56:58 -0700

    Added basic concept of sorted items for checklist

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -3,5 +3,40 @@ class ChecklistsController < ApplicationController
   end
 
   def new
+    @checklist = Checklist.new
+  end
+
+  def create
+    c = Checklist.new 
+
+    c.first_middle_name = params["first_middle_name"]
+    c.last_name = params["last_name"]
+    c.birth_year = params["birth_year"]
+    c.birth_location = params["birth_location"]
+
+    lived_in = ""
+    if params["state_1"]
+      lived_in << params["state_1"]
+    end
+    if params["state_2"]
+      lived_in << ","
+      lived_in << params["state_2"]
+    end
+    if params["state_3"]
+      lived_in << ","
+      lived_in << params["state_3"]
+    end
+    c.lived_in = lived_in
+    
+    c.death_year = params["death_year"]
+    c.death_location = params["death_location"]
+
+    c.save!
+
+    redirect_to checklist_path(c)
+  end
+
+  def show
+    @checklist = Checklist.find(params[:id])
   end
 end
--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,2 +1,61 @@
 class Checklist < ActiveRecord::Base
+  attr_accessible :first_middle_name, :last_name, :birth_year, :birth_location, :lived_in, :death_year, :death_location
+
+  def state(index)
+    if lived_in
+      states = lived_in.split(",")
+      if states[index - 1]
+        states[index - 1]
+      else
+        nil
+      end
+    else
+      nil
+    end
+  end
+
+  def items
+    @items = []
+
+    # Birth 
+    @items << { :name => "Birth Certificate/Record", :year => birth_year }
+
+    # US Census Records
+    first_census_year = birth_year + (10 - birth_year.to_s[3].to_i) 
+    last_census_year = death_year - (death_year.to_s[3].to_i)     
+
+    year = first_census_year
+    while year <= last_census_year
+      @items << { :name => "#{year} U.S. Census", :year => year}
+      year += 10
+    end
+
+    # Marriage Record
+    @items << { :name => "Marriage Certificate/Record", :year => birth_year + 25 }
+
+    # State Census
+    for i in 1..3
+      lived_in_state = state(i)
+      state_records = State.find_by_code(lived_in_state).census_reports
+      if state_records
+        state_records.each do |sr|
+          if sr.year >= birth_year && sr.year <= death_year
+            @items << { :name => "#{sr.year} #{sr.state.code} Census", :year => sr.year }
+          end
+        end
+      end
+    end      
+
+    # Death Record
+    @items << { :name => "Death Certificate/Record", :year => death_year }
+
+    # Cemetery/Obituary
+    @items << { :name => "Cemetery, Obituary", :year => death_year + 1 }
+
+    @items.sort! { |a,b| a[:year] <=> b[:year] }
+    @items
+
+
+  end
+
 end
