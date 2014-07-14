674564ebdbf2c96ec810f34dd01b49465cf77fe1
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2014-07-14 08:21:51 -0700

    Updated to have grouped sorted items

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -19,9 +19,16 @@ class Checklist < ActiveRecord::Base
 
   def items
     @items = []
+    @grouped_items = { 
+      :vital_records => [], 
+      :us_census_records => [],
+      :state_census_records => [],
+      :military_records => [],
+      :other_records => []
+    }
 
     # Birth 
-    @items << { :name => "Birth Record", :year => birth_year }
+    @items << { :group => "Vital Records", :name => "Birth Record", :year => birth_year }
 
     # US Census Records
     first_census_year = birth_year + (10 - birth_year.to_s[3].to_i) 
@@ -42,12 +49,12 @@ class Checklist < ActiveRecord::Base
 
     year = first_census_year
     while year <= last_census_year
-      @items << { :name => "#{year} U.S. Census", :year => year}
+      @items << { :group => "U.S. Census Records", :name => "#{year} U.S. Census", :year => year}
       year += 10
     end
 
     # Marriage Record
-    @items << { :name => "Marriage Record", :year => birth_year + 25 }
+    @items << { :group => "Vital Records", :name => "Marriage Record", :year => birth_year + 25 }
 
     # State Census
     unless lived_in.blank?
@@ -58,7 +65,7 @@ class Checklist < ActiveRecord::Base
           if state_records
             state_records.each do |sr|
               if sr.year >= birth_year && sr.year <= end_year
-                @items << { :name => "#{sr.year} #{sr.state.code} Census", :year => sr.year }
+                @items << { :group => "State Census Records", :name => "#{sr.year} #{sr.state.code} Census", :year => sr.year }
               end
             end
           end
@@ -67,31 +74,47 @@ class Checklist < ActiveRecord::Base
     end
 
     # Death Record
-    @items << { :name => "Death Record", :year => end_year }
+    @items << { :group => "Vital Records", :name => "Death Record", :year => end_year }
 
     # Cemetery/Obituary
-    @items << { :name => "Cemetery, Obituary", :year => end_year + 1 }
+    @items << { :group => "Vital Records", :name => "Cemetery, Obituary", :year => end_year + 1 }
 
     # War records
     if birth_year <= 1775 and end_year >= 1775
-      @items << { :name => "1775-1783 Revolutionary War", :year => 1775 }
+      @items << { :group => "Military Records", :name => "1775-1783 Revolutionary War", :year => 1775 }
     end
     if birth_year <= 1812 and end_year >= 1812
-      @items << { :name => "1812-1815 War of 1812", :year => 1812 } 
+      @items << { :group => "Military Records", :name => "1812-1815 War of 1812", :year => 1812 } 
     end
     if birth_year <= 1861 and end_year >= 1861
-      @items << { :name => "1861-1865 Civil War", :year => 1861 }
+      @items << { :group => "Military Records", :name => "1861-1865 Civil War", :year => 1861 }
     end
     if birth_year <= 1914 and end_year >= 1914
-      @items << { :name => "1914-1918 World War I", :year => 1914 }
+      @items << { :group => "Military Records", :name => "1914-1918 World War I", :year => 1914 }
     end
     if birth_year <= 1939 and end_year >= 1939
-      @items << { :name => "1939-1945 World War II", :year => 1939 }
+      @items << { :group => "Military Records", :name => "1939-1945 World War II", :year => 1939 }
     end
 
-    @items.sort! { |a,b| a[:year] <=> b[:year] }
-    @items
+    # Other Records
+    @items << { :group => "Other Records", :name => "Immigration", :year => 2000 }
+    @items << { :group => "Other Records", :name => "Tax", :year => 2001 }
+    @items << { :group => "Other Records", :name => "Other", :year => 2002 }
 
+    # To Do Records
+    @items << { :group => "To Do Records", :name => "", :year => 2003 }
+    @items << { :group => "To Do Records", :name => "", :year => 2004 }
+
+    @items.sort! { |a,b| a[:year] <=> b[:year] }
+
+    @grouped_items[:vital_records] = @items.select { |i| i[:group] == "Vital Records"}.dup
+    @grouped_items[:us_census_records] = @items.select { |i| i[:group] == "U.S. Census Records"}.dup
+    @grouped_items[:state_census_records] = @items.select { |i| i[:group] == "State Census Records"}.dup
+    @grouped_items[:military_records] = @items.select { |i| i[:group] == "Military Records"}.dup
+    @grouped_items[:other_records] = @items.select { |i| i[:group] == "Other Records"}.dup
+    @grouped_items[:to_do_records] = @items.select { |i| i[:group] == "To Do Records"}.dup
+
+    [@items,@grouped_items]
 
   end
 
